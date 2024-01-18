318ed24ed5917efad73263a94194b79c84ba0fb6
dntai <donhutai@gmail.com>
2024-01-18 13:44:11 +0700

    clouds/cloud_setup.py startsss

--- resyslab_utils/clouds/cloud_setup.py
+++ resyslab_utils/clouds/cloud_setup.py
@@ -17,50 +17,54 @@ def start_ssh(id_rsa_pub = "", password = "", install_ssh = False, config_ssh = 
     Modified: 2024/01/18
     Created : 2024/01/18
     """    
+    from IPython import get_ipython
+    import os
     print(f'{"*" * 10} SETUP SSH SERVICE {"*"*10}')
 
     if install_ssh is True:
-        !echo "> Install ssh service..."
-        !apt-get install ssh -y 2>&1 > /dev/null
+        get_ipython().system('echo "> Install ssh service..."')
+        get_ipython().system('apt-get install ssh -y 2>&1 > /dev/null')
     
     if id_rsa_pub != "":
-        !echo "> Copy public key to authorized keys..."
-        !mkdir -p ~/.ssh
-        !echo $id_rsa_pub > ~/.ssh/authorized_keys
+        get_ipython().system('echo "> Copy public key to authorized keys..."')
+        get_ipython().system('mkdir -p ~/.ssh')
+        get_ipython().system(f'echo {id_rsa_pub} > ~/.ssh/authorized_keys')
 
     if config_ssh is True:
-        !echo "> Config ssh service..."
-        !sed -i 's/^#Port.*/Port 22/' /etc/ssh/sshd_config
-        !sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#Port.*/Port 22/' /etc/ssh/sshd_config
-        !sed -i 's/^#ListenAddress 0.*/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
-        !sed -i 's/^#ListenAddress ::.*/ListenAddress ::/' /etc/ssh/sshd_config
+        get_ipython().system('echo "> Config ssh service..."')
+        get_ipython().system("sed -i 's/^#Port.*/Port 22/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#Port.*/Port 22/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#ListenAddress 0.*/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#ListenAddress ::.*/ListenAddress ::/' /etc/ssh/sshd_config")
 
-        !sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+        get_ipython().system("sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config")
 
-        !sed -i 's/^#AllowAgentForwarding.*/AllowAgentForwarding yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#AllowTcpForwarding.*/AllowTcpForwarding yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#PermitTTY.*/PermitTTY yes/' /etc/ssh/sshd_config
-        !sed -i 's/^#GatewayPorts.*/GatewayPorts yes/' /etc/ssh/sshd_config
-        !systemctl reload sshd
+        get_ipython().system("sed -i 's/^#AllowAgentForwarding.*/AllowAgentForwarding yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#AllowTcpForwarding.*/AllowTcpForwarding yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#PermitTTY.*/PermitTTY yes/' /etc/ssh/sshd_config")
+        get_ipython().system("sed -i 's/^#GatewayPorts.*/GatewayPorts yes/' /etc/ssh/sshd_config")
+        # !systemctl reload sshd
 
     if password != "":
-        !echo "> Set root password..."
-        !echo -e "$password\n$password" | passwd root >/dev/null 2>&1
+        get_ipython().system('echo "> Set root password..."')
+        get_ipython().system('echo -e "$password\n$password" | passwd root >/dev/null 2>&1')
 
-    !echo "> Restart SSH service..."
-    !service ssh restart 
+    get_ipython().system('echo "> Restart SSH service..."')
+    get_ipython().system('service ssh restart')
     print(f"")
 
-    !echo "> Process ~/.bashrc for PS1, TERM..."
-    !src="PS1=" && echo "$src" \
-        && dest="PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" && echo "$dest" \
-        && sed -i "s/$(echo $src | sed -e 's/\([[\/.*]\|\]\)/\\&/g').*/$(echo $dest | sed -e 's/[\/&]/\\&/g')/g" ~/.bashrc
-    !grep -qx "^TERM=.*$" ~/.bashrc || echo "TERM=xterm-256color" >> ~/.bashrc
+    get_ipython().system('echo "> Process ~/.bashrc to registry PS1, TERM..."')
+    get_ipython().system('grep -qx "^PS1=.*$" ~/.bashrc || echo "PS1=" >> ~/.bashrc')
+    dest = "PS1='\\[\\e]0;\\u@\h: \\w\\a\\]${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\$ '"
+    cmd = "sed -i \"s/$(echo $src | sed -e 's/\\([[\\/.*]\\|\\]\\)/\\\\&/g').*/$(echo $dest | sed -e 's/[\\/&]/\\\\&/g')/g\" ~/.bashrc"
+    get_ipython().system(f'src="PS1=" && echo $src && dest="{dest}" && echo "$dest" && {cmd}')
+
+    cmd = 'grep -qx "^TERM=.*$" ~/.bashrc || echo "TERM=xterm-256color" >> ~/.bashrc'
+    get_ipython().system(f'{cmd}')
     print(f"")
     
     print(f'{"-" * 10} Finished {"-"*10}\n')
     pass # start_ssh
-
